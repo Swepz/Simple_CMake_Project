3671798e4d63147177b0de9fa2c1c217b0c9a67e
swepz <taza@live.se>
2020-06-14 12:51:22 +0200

    Check if test dir exists

--- source/main.c
+++ source/main.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+#include "linked_list.c"
+
 int main() {
+        printf("VAR*2 = %d\n", testA(10));
         return 0;
 };
--- test/test.c
+++ test/test.c
@@ -1,5 +1,6 @@
+#include "linked_list.c"
 #include "main.h"
-
 int main() {
+        printf("VAR*2 = %d\n", testA(5));
         return 0;
 };
