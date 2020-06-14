0c66f032b382b11cdade47f056a37ed44302164f
swepz <taza@live.se>
2020-06-14 09:59:19 +0200

    Modified CMakeLists

--- source/main.c
+++ source/main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
 int main() {
-        printf("Hello ");
         return 0;
 };
--- test/test.c
+++ test/test.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
 int main() {
-        printf("World\n");
         return 0;
 };
